b3e1d7a8525a92a394a8fdb920e16d30af064194
Sinkandika <andika.alstam@gmail.com>
2026-08-31 20:17:48 +0700

    Fix image size and next/previous button

--- client/src/components/ImagePreviewModal.jsx
+++ client/src/components/ImagePreviewModal.jsx
@@ -1,5 +1,5 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
@@ -73,47 +73,51 @@ function ImagePreviewModal ({
     className="fixed inset-0 bg-black/50 flex flex-col items-center justify-center z-99"
     >
 
-      <button
-      onClick={onClose}
-      className="absolute right-0 top-0"
-      >
-        ✕
-      </button>
-
       {images.length > 1 ? (
         <div 
         onClick={(e) => e.stopPropagation()}
-        className="flex flex-col"
+        className="flex flex-row justify-center items-center gap-x-2"
         >
           <button
             onClick={previousImage}
-            className="absolute left-2 top-1/2 -translate-y-1/2 z-10 bg-black/10 hover:bg-black/60 text-white rounded-full w-7 h-7 md:w-10 md:h-10 transition flex items-center justify-center"
+            className="bg-black/10 hover:bg-black/60 text-white rounded-full w-7 h-7 md:w-10 md:h-10 transition flex items-center justify-center"
           >
             <ChevronLeft className="w-4 h-4 md:w-6 md:h-6" />
           </button>
 
-          <AnimatePresence mode="wait" custom={direction}>
-            <motion.img
-              key={images[previewImage]?.id}
-              custom={direction}
-              src={images[previewImage]?.image_url}
-              alt="Preview"
-              className="max-w-full max-h-screen object-contain rounded-lg"
-              variants={imageVariants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                duration: 0.1,
-                ease: "easeInOut",
-              }}
-            />
-          </AnimatePresence>
+          <div className="flex">
+            <AnimatePresence mode="wait" custom={direction}>
+              <motion.img
+                key={images[previewImage]?.id}
+                custom={direction}
+                src={images[previewImage]?.image_url}
+                alt="Preview"
+                className="max-w-full max-h-screen object-contain rounded-lg"
+                variants={imageVariants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  duration: 0.1,
+                  ease: "easeInOut",
+                }}
+              />
+            </AnimatePresence>
+
+            <div className="relative">
+              <button
+              onClick={onClose}
+              className="absolute right-1 top-2 text-secondary hover:text-black"
+              >
+                <X/>
+              </button>
+            </div>
+          </div>
 
           <div>
             <button
               onClick={nextImage}
-              className="absolute right-2 top-1/2 -translate-y-1/2 z-10 bg-black/10 hover:bg-black/60 text-white rounded-full w-7 h-7 md:w-10 md:h-10 transition flex items-center justify-center"
+              className="bg-black/10 hover:bg-black/60 text-white rounded-full w-7 h-7 md:w-10 md:h-10 transition flex items-center justify-center"
             >
               <ChevronRight className="w-4 h-4 md:w-6 md:h-6" />
             </button>
@@ -125,7 +129,7 @@ function ImagePreviewModal ({
             <img 
             src={images[previewImage]?.image_url}
             alt="Preview"
-            className="max-w-full max-h-screen object-contain rounded-lg"
+            className="w-4xl max-h-screen object-contain rounded-lg"
             />
         </div>
       )}
